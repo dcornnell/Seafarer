0ba0d96089332565f2103f0d0d6cb4f783792089
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-23 14:29:11 -0500

    added a basic map

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
+import L from "leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import "./App.css";
-import Map from "./components/Map/Map";
 
-class App extends Component {
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+export default class SimpleExample extends Component<{}, State> {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13
+  };
+
   render() {
-    return <Map></Map>;
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map className="map" center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
   }
 }
-
-export default App;
--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -7,10 +7,12 @@ class Map extends React.Component {
   componentDidMount() {
     this.map = L.map("mapid").setView([51.505, -0.09], 13);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
+      maxZoom: 20,
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
+
+    var marker = L.marker([51.5, -0.09]).addTo(this.map);
   }
   render() {
     return <div id="mapid"></div>;
