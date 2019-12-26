43639053f2ecc26ea5da33a37b12c213f6180439
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-26 17:11:23 -0500

    got the map to center on all the points

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,10 @@ import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import axios from "axios";
 
 let DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow
+  iconUrl:
+    "https://raw.githubusercontent.com/iconic/open-iconic/master/png/map-marker-8x.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32]
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
@@ -21,15 +23,28 @@ class App extends Component {
     lat: 51.505,
     lng: -0.09,
     zoom: 13,
-    events: []
+    events: [],
+    min_bounds: [],
+    max_bounds: []
   };
 
   generatePoints() {
     axios.get("/journeys/5dfc26f23a7cb0d6b324d79a").then(res => {
       const events = res.data.ships[0].events;
+      const boundsArray = [];
+      events.map(function(event) {
+        boundsArray.push([
+          event.location.coordinates[1],
+          event.location.coordinates[0]
+        ]);
+      });
+      const bounds = L.bounds(boundsArray);
 
-      this.setState({ events });
-      console.log(this.state.events);
+      this.setState({
+        events: events,
+        min_bounds: [bounds.min.x, bounds.min.y],
+        max_bounds: [bounds.max.x, bounds.max.y]
+      });
     });
   }
 
@@ -38,31 +53,35 @@ class App extends Component {
   }
 
   render() {
-    const position = [this.state.lat, this.state.lng];
-    return (
-      <>
-        <Map className="map" center={position} zoom={this.state.zoom}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {this.state.events.map(event => {
-            const position = [
-              event.location.coordinates[1],
-              event.location.coordinates[0]
-            ];
-            return (
-              <Marker position={position}>
-                <Popup>{event.description}</Popup>
-              </Marker>
-            );
-          })}
-          <Marker position={position}>
-            <Popup></Popup>
-          </Marker>
-        </Map>
-      </>
-    );
+    if (this.state.max_bounds.length > 0 && this.state.min_bounds.length > 0) {
+      console.log(this.state.min_bounds);
+      console.log(this.state.max_bounds);
+      return (
+        <>
+          <Map
+            className="map"
+            bounds={[this.state.min_bounds, this.state.max_bounds]}
+          >
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {this.state.events.map(event => {
+              const position = [
+                event.location.coordinates[1],
+                event.location.coordinates[0]
+              ];
+
+              return (
+                <Marker position={position}>
+                  <Popup>{event.description}</Popup>
+                </Marker>
+              );
+            })}
+          </Map>
+        </>
+      );
+    } else return null;
   }
 }
 
