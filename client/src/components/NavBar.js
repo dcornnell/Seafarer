295f322ebe7ca2413552f00cbf358c063f1c86c1
Daniel Cornnell <daniel.cornnell@gmail.com>
2020-01-03 22:00:59 -0500

    added a new journey button on the main page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { TopBar, TopBarLeft } from "react-foundation";
-
+import { TopBar, TopBarLeft, TopBarRight } from "react-foundation";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <TopBar>
       <TopBarLeft>Seafarer</TopBarLeft>
+      <TopBarRight>
+        <Link to={"/journeys/new"}>New Journey</Link>
+      </TopBarRight>
     </TopBar>
   );
 }
