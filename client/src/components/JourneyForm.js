90265d22438cabe131beb3cf3f73b81d8dbed714
Daniel Cornnell <daniel.cornnell@gmail.com>
2020-01-03 21:52:23 -0500

    added the newjourney page and the ships component

--- client/src/components/JourneyForm.js
+++ client/src/components/JourneyForm.js
@@ -1,5 +1,8 @@
 import React from "react";
 import axios from "axios";
+import Modal from "./Modal";
+import ShipForm from "./ShipForm";
+import { Redirect } from "react-router-dom";
 
 class JourneyForm extends React.Component {
   state = {
@@ -8,7 +11,9 @@ class JourneyForm extends React.Component {
     description: "",
     start_date: "",
     end_date: "",
-    selectedShips: []
+    selectedShips: [],
+    modal: false,
+    formSubmitted: false
   };
 
   handleInputChange = event => {
@@ -25,9 +30,16 @@ class JourneyForm extends React.Component {
   getShips() {
     axios.get("/ships/all").then(res => {
       this.setState({ allShips: res.data });
-      console.log(this.state.allShips);
     });
   }
+
+  componentDidUpdate() {
+    this.getShips();
+  }
+  toggleModal = event => {
+    event.preventDefault();
+    this.setState({ modal: !this.state.modal });
+  };
 
   handleSelectShip = option => {
     const ship = JSON.parse(option.target.value);
@@ -41,14 +53,20 @@ class JourneyForm extends React.Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-    console.log(this.state);
+
+    const shipIds = [];
+    this.state.selectedShips.map(ship => {
+      shipIds.push(ship._id);
+      return null;
+    });
 
     axios
       .post("/journeys/create", {
         name: this.state.name,
         description: this.state.description,
         start_date: this.state.start_date,
-        end_date: this.state.end_date
+        end_date: this.state.end_date,
+        ships: shipIds
       })
       .then(function(response) {
         console.log(response);
@@ -61,57 +79,72 @@ class JourneyForm extends React.Component {
       name: "",
       description: "",
       start_date: "",
-      end_date: ""
+      end_date: "",
+      formSubmitted: true
     });
   };
 
   render() {
+    if (this.state.formSubmitted) {
+      return <Redirect to="/" />;
+    }
     return (
-      <form className="form">
-        Journeys Name
-        <input
-          value={this.state.name}
-          name="name"
-          onChange={this.handleInputChange}
-          type="text"
-          placeholder="name"
-        />
-        Ships on the Journey:
-        {this.state.selectedShips.map(ship => {
-          return <p key={ship._id}>{ship.name}</p>;
-        })}
-        <select value={this.state.ships} onChange={this.handleSelectShip}>
-          {this.state.allShips.map(ship => {
-            return (
-              <option key={ship._id} value={JSON.stringify(ship)}>
-                {ship.name}
-              </option>
-            );
+      <>
+        <form className="form">
+          Journeys Name
+          <input
+            value={this.state.name}
+            name="name"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="name"
+          />
+          Ships on the Journey:
+          {this.state.selectedShips.map(ship => {
+            return <p key={ship._id}>{ship.name}</p>;
           })}
-        </select>
-        <input
-          value={this.state.description}
-          name="description"
-          onChange={this.handleInputChange}
-          type="text"
-          placeholder="description"
-        />
-        <input
-          value={this.state.start_date}
-          name="start_date"
-          onChange={this.handleInputChange}
-          type="date"
-          placeholder="start_date"
-        />
-        <input
-          value={this.state.end_date}
-          name="end_date"
-          onChange={this.handleInputChange}
-          type="date"
-          placeholder="end_date"
-        />
-        <button onClick={this.handleFormSubmit}>Submit</button>
-      </form>
+          <button onClick={this.toggleModal}>add a ship</button>
+          <select value={this.state.ships} onChange={this.handleSelectShip}>
+            {this.state.allShips.map(ship => {
+              return (
+                <option key={ship._id} value={JSON.stringify(ship)}>
+                  {ship.name}
+                </option>
+              );
+            })}
+          </select>
+          <input
+            value={this.state.description}
+            name="description"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="description"
+          />
+          <input
+            value={this.state.start_date}
+            name="start_date"
+            onChange={this.handleInputChange}
+            type="date"
+            placeholder="start_date"
+          />
+          <input
+            value={this.state.end_date}
+            name="end_date"
+            onChange={this.handleInputChange}
+            type="date"
+            placeholder="end_date"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+        <Modal
+          status={this.state.modal}
+          onClick={event => {
+            this.toggleModal(event);
+          }}
+        >
+          <ShipForm />
+        </Modal>
+      </>
     );
   }
 }
---
+++ client/src/components/ShipForm.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class ShipForm extends Component {
+  state = {
+    name: "",
+    type: "",
+    about: "",
+    country: ""
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+
+    axios
+      .post("/ships/create", {
+        name: this.state.name,
+        type: this.state.type,
+        about: this.state.about,
+        country: this.state.country
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    this.setState({
+      name: "",
+      type: "",
+      about: "",
+      country: ""
+    });
+  };
+
+  render() {
+    return (
+      <form className="form">
+        Ship Name:
+        <input
+          value={this.state.name}
+          name="name"
+          onChange={this.handleInputChange}
+          type="text"
+          placeholder="name"
+        />
+        Type:
+        <input
+          value={this.state.type}
+          name="type"
+          onChange={this.handleInputChange}
+          type="text"
+          placeholder="type"
+        />
+        About:
+        <input
+          value={this.state.about}
+          name="about"
+          onChange={this.handleInputChange}
+          type="text"
+          placeholder="about"
+        />
+        Country of Origin:
+        <input
+          value={this.state.country}
+          name="country"
+          onChange={this.handleInputChange}
+          type="text"
+          placeholder="country"
+        />
+        <button onClick={this.handleFormSubmit}>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default ShipForm;
