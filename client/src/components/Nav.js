2720a013f2b8c780e1df8d6ac0deb3b3255ea77e
Daniel Cornnell <daniel.cornnell@gmail.com>
2020-01-09 22:02:10 -0500

    added logo

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../icons/Logo_white.png";
 
 import { Link } from "react-router-dom";
 function Nav({ loggedIn }) {
@@ -8,7 +9,11 @@ function Nav({ loggedIn }) {
       role="navigation"
       aria-label="main navigation"
     >
-      <div className="navbar-brand">SeaFarers</div>
+      <div className="navbar-brand">
+        <Link className="navbar-item" to={"/"}>
+          <img src={logo} width="100" height="100" alt="logo" />
+        </Link>
+      </div>
 
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
